c1a3c0ac5df092c00e825ff70277f91391d001bd
Edward Spencer <epfspencer@yahoo.com>
2021-02-02 22:10:21 +0000

    Add models for likes and posts

--- network/models.py
+++ network/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        'User', on_delete=models.CASCADE, related_name='posts'
+    )
+    content = models.CharField(max_length=140)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        'User', on_delete=models.CASCADE, related_name='likes'
+    )
+    post = models.ForeignKey(
+        'Post', on_delete=models.CASCADE, related_name='likes'
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
