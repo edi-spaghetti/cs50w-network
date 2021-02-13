55fe588b7d4f0a446264edadf7598ea6adabd7a5
Edward Spencer <epfspencer@yahoo.com>
2021-02-13 18:33:41 +0000

    Add filter functionality
    A limited set of filters can now be made through the api.
    Linked fields (e.g. User.posts) seem to be throwing an error which needs
    to be fixed. Also the react components needs to be updated to query with
    this filter syntax and render the correct values.

--- network/utils.py
+++ network/utils.py
@@ -1,14 +1,80 @@
+import re
 import json
 
 from django.db.models import Model
+from django.db.models.base import ModelBase
+from django.db.models.fields.related import RelatedField
+from django.db.models.fields import IntegerField
 
 
-def parse_filters(model, filter_):
+OPERATORS = {
+    # TODO: refactor operators as class
+    # op   db lookup   is_include
+    '==': ('exact',    True),
+    '!=': ('exact',    False),
+}
+
+
+def build_filter_regex(fields):
+    """
+    Build and compile a regex pattern used to parse filters from api
+    :param fields:
+    :return:
+    """
+    f = '|'.join(fields)
+    o = '|'.join(OPERATORS.keys())
+    reg = re.compile(f'({f})\s*({o})\s*(.+)')
+    return reg
+
+
+def convert_filter_value_by_data_type(field, value):
+    """
+    Converts value passed into an api filter to an appropriate data type
+    for that field type. Currently only supports strings and integers.
+    :param field: Django model field type
+    :param value: Value to convert
+    :return: Convert value
+    """
+
+    is_int = isinstance(field, IntegerField)
+    is_int_like = isinstance(field, RelatedField)
+    if is_int or is_int_like:
+        value = int(value)
+
+    return value
+
+
+def parse_filters(model, filters):
+    """
+    Parses incoming filters that can be used filter to models either
+    positively or negatively. Filters should come in as a list of strings of
+    format `<field> <operator> <value>` e.g. `id == 1` or `id!=1` (note,
+    whitespace between terms is optional).
+    Fields will be sanitised, and must be valid for the model provided.
+    Currently supported operators are:
+        == -> positively filter field by value
+        != -> negatively filter field by value
+    Values will be converted to the appropriate data type for the field.
+    Currently supported data types are:
+        str (CharField)
+        int (NumberField, PrimaryKey)
+    :type model: :class:`Model`
+    :param filters:
+    :return: Dictionaries arranged into a format appropriate to pass as
+             keywords to :meth:`Model.filter` and :meth:`Model.exclude`.
+    :rtype: tuple[dict, dict]
+    :raises: ValueError if any filter is cannot be parsed, whether through
+             invalid fields or bad value conversion.
+    """
+
+    # validate model is correct type
+    if not isinstance(model, (ModelBase, ModelExtension)):
+        raise ValueError(f'expected extended model, got {type(model)}')
 
     # validate filter is a list of strings
-    if not isinstance(filter_, list):
-        raise ValueError(f'Filter must be list: got {type(filter_)}')
-    for i, f in enumerate(filter_):
+    if not isinstance(filters, list):
+        raise ValueError(f'filter must be list: got {type(filters)}')
+    for i, f in enumerate(filters):
         if not isinstance(f, str):
             raise ValueError(
                 f'Filter items must be strings, got {type(f)} at index {i}'
@@ -17,10 +83,29 @@ def parse_filters(model, filter_):
     filter_dict = dict()
     exclude_dict = dict()
 
-    # TODO: convert api filter syntax into django filter syntax
-    #       e.g. field==value -> filter_dict[field__exact] = value
-    #       e.g. field!=value -> exclude_dict[field__exact] = value
-    # REF: https://docs.djangoproject.com/en/dev/ref/models/querysets/#field-lookups
+    # TODO: support for filtering on non-db fields
+    fields = model.default_fields()
+    reg = build_filter_regex(fields)
+
+    for f in filters:
+        try:
+            field, operator, value = reg.search(f).groups()
+        except AttributeError:
+            # TODO: more useful feedback on parse filter failure
+            raise ValueError(f'failed to parse filter: {f}')
+
+        lookup, is_include = OPERATORS[operator]
+        lookup_key = f'{field}__{lookup}'
+
+        # TODO: data type manager
+        db_field = getattr(model, field).field
+        value = convert_filter_value_by_data_type(db_field, value)
+
+        # TODO: warning if duplicate filters provided
+        if is_include:
+            filter_dict[lookup_key] = value
+        else:
+            exclude_dict[lookup_key] = value
 
     return filter_dict, exclude_dict
 
@@ -92,6 +177,7 @@ class ModelExtension(object):
         return context
 
     def sanitize_return_fields(self, fields):
+        # TODO: refactor sanitisation to expect list of strings
         # if fields is None, this will set it to a null string
         fields = fields or ''
         if fields == self.SELECT_ALL:
@@ -148,6 +234,7 @@ class ModelExtension(object):
     def serializable_value(self, field_name):
         # NOTE: by the time self gets used, it will be multiply inheriting from
         #       :class:`Model`, so this will not throw an error.
+        # TODO: fix serialization of linked fields
         value = Model.serializable_value(self, field_name)  # noqa
         try:
             json.dumps(value)
