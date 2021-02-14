6927ff952e5f1bf53b5f9a698b9effce115480b7
Edward Spencer <epfspencer@yahoo.com>
2021-02-14 16:58:29 +0000

    Add user tests
    Just some basic tests to verify the serialization methods I added are
    working. Didn't want to do tests for all methods on all models because
    I've already left setting up tests a bit late and I've got better things
    to do. I'll get around to adding more as I go from now on, promise.

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,85 @@
 from django.test import TestCase
+from .models import User, Post, Like
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user('test', password='test')
+        self.user2 = User.objects.create_user('test2', password='test2')
+        self.post = Post.objects.create(user=self.user, content='Test Post')
+        self.like = Like.objects.create(user=self.user2, post=self.post)
+
+
+class UserTests(ModelTests):
+
+    def test_serialize_direct_field(self):
+        self.assertEqual(self.user.serialize(['id']), {'id': 1})
+
+    def test_serialize_linked_field(self):
+        self.assertEqual(
+            self.user.serialize([{'posts': ['id']}]),
+            {'posts': [{'id': 1}]}
+        )
+
+    def test_serialize_all_direct(self):
+        serialized = self.user.serialize(True)
+        expected_keys = [
+                'is_staff', 'is_active', 'leader_count', 'is_superuser',
+                'last_name', 'id', 'last_login', 'first_name',
+                'follower_count', 'email', 'password', 'date_joined',
+                'can_follow', 'is_following', 'username'
+        ]
+
+        for key in expected_keys:
+            self.assertTrue(key in serialized)
+
+    def test_serialize_invalid_direct_field(self):
+        with self.assertRaises(ValueError) as e:
+            _ = self.user.serialize(['invalid'])
+
+        # check we get a useful error message
+        self.assertEqual(
+            str(e.exception),
+            f'invalid is not a direct field of {User}'
+        )
+
+    def test_serialize_invalid_linked_field(self):
+        with self.assertRaises(ValueError) as e:
+            _ = self.user.serialize([{'invalid': []}])
+
+        # check we got a useful error message
+        self.assertEqual(
+            str(e.exception),
+            f'invalid is not a linked field of {User}'
+        )
+
+    def test_serialize_direct_as_linked_field(self):
+        with self.assertRaises(ValueError) as e:
+            _ = self.user.serialize([{'username': []}])
+
+        # check we got a useful error message
+        self.assertEqual(
+            str(e.exception),
+            f'username is not a linked field of {User}'
+        )
+
+    def test_serialize_invalid_type_outer(self):
+        with self.assertRaises(ValueError) as e:
+            _ = self.user.serialize({'invalid'})
+
+        self.assertEqual(
+            str(e.exception),
+            f'valid types are list[str, dict] or {User.SELECT_ALL} '
+            f'- got {type(set())}'
+        )
+
+    def test_serialize_invalid_type_inner(self):
+        with self.assertRaises(ValueError) as e:
+            _ = self.user.serialize([{'invalid'}])
+
+        # check we got a useful error message
+        self.assertEqual(
+            str(e.exception),
+            f'expected inner items str or dict - got {type(set())}'
+        )
